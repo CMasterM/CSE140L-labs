c8e0847aa031aa26ec5917bfee9147e8acb52686
Connor Malley <97553498+CMasterM@users.noreply.github.com>
2023-10-16 18:22:00 -0700

    Add files via upload

---
+++ Lab1/Lab_1_Verilog_starter_code_export/mux2.sv
@@ -0,0 +1,22 @@
+// 2:1 mux (selector) of N-wide buses
+// CSE140L
+module mux2 #(parameter WIDTH = 8)
+ (input        [WIDTH-1:0] d0, d1, 
+  input                    s, 
+  output logic [WIDTH-1:0] y);
+// fill in guts
+// combinational (unclocked) logic -- use =, not <=
+ always_comb begin
+  if(s) y = d0;
+  else y = d1;
+
+ end
+ 
+
+// s   y
+// 0   d0	y[7:0] = d0[7:0]
+// 1   d1	y[7:0] = d1[7:0]
+
+endmodule
+
+
